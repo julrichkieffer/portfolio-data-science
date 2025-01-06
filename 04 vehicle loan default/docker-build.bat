a4855edf25d87d710389a2fc67b5d2e75693d594
Julrich Kieffer <julrich@mrkieffer.london>
2025-01-06 14:18:14 +0000

    Docker image & documentation

---
+++ 04 vehicle loan default/docker-build.bat
@@ -0,0 +1,1 @@
+docker build -t loan-default-prediction:capstone-1 .
---
+++ 04 vehicle loan default/docker-run.bat
@@ -0,0 +1,3 @@
+@REM docker run -it --rm --entrypoint=/bin/ash loan-default-prediction:capstone-1
+@REM docker run -it --rm --entrypoint=bash loan-default-prediction:capstone-1
+docker run -it --rm -p 8000:8000 loan-default-prediction:capstone-1
